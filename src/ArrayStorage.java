44426876aadd75af09859f89a4ac11fa52df590a
alsmirnoff <alex-sm-91@mail.ru>
2023-10-02 22:34:52 +0300

    simplifying code after mentor help

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,64 +2,49 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10];
+    Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        int i = 0;
-        while (storage[i] != null) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
-            i++;
         }
     }
 
     void save(Resume r) {
-        for(int i = 0; i < storage.length; i++){
-            if(storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        int i = 0;
-        while (storage[i] != null) {
-            if(storage[i].uuid.equals(uuid)) return storage[i];
-            i++;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
-        for(int i = 0; i < this.size(); i++){
-            if(uuid.equals(storage[i].uuid)) {
-                for (int k = i; k < this.size() - 1; k++) {
-                    storage[k] = storage[k+1];
-                }
-                break;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
             }
         }
-        storage[this.size() - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] shortStorage = new Resume[this.size()];
-        for(int i = 0; i < shortStorage.length; i++){
-            shortStorage[i] = storage[i];
+        Resume[] allResume = new Resume[size];
+        for (int i = 0; i < allResume.length; i++) {
+            allResume[i] = storage[i];
         }
-        return shortStorage;
+        return allResume;
     }
 
     int size() {
-        int i = 0;
-        int res = 0;
-        while(i < storage.length && storage[i] != null){
-            res++;
-            i++;
-        }
-        return res;
+        return size;
     }
 }
