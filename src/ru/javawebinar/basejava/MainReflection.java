88f78d9a0f336c66bb0c29c8e8e23d284bb13e31
alsmirnoff <alex-sm-91@mail.ru>
2024-10-16 21:58:25 +0300

    lesson 4 invoke method via Reflection

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,15 +3,26 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
+        Class clazz = r.getClass();
+        /*Method [] methods = clazz.getMethods();
+        for (Method method : methods) {
+            System.out.println("method name: " + method.getName() );
+        }*/
+        
+        Method method = clazz.getMethod("toString");
+        System.out.println("toString invoke: " + method.invoke(r));
+
         // TODO : invoke r.toString via Reflection
         System.out.println(r);
     }
