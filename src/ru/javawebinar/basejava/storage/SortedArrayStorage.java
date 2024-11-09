910cb57d8395bec628b2692929833d42e9ea1c68
alsmirnoff <alex-sm-91@mail.ru>
2024-11-09 21:43:16 +0300

    lesson 6 trying nested classes

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -3,11 +3,27 @@ package ru.javawebinar.basejava.storage;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Sorted array based storage for Resumes
  */
 public class SortedArrayStorage extends AbstractArrayStorage{
+
+    // private static class ResumeComparator implements Comparator<Resume> {
+    //     @Override
+    //     public int compare(Resume o1, Resume o2) {
+    //         return o1.getUuid().compareTo(o2.getUuid());
+    //     }
+    // }
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    };
+
 
     @Override
     protected void insertElement(Resume resume, int index) {
@@ -27,6 +43,7 @@ public class SortedArrayStorage extends AbstractArrayStorage{
     @Override
     protected Integer getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
+
 }
